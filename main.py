4886d7c2a2e1305916a4f6804667d58ddfed454d
ZetkoOfficial <info@zetkoofficial.com>
2025-07-16 18:18:40 +0200

    ocenjene konstante in dodani grafi

--- main.py
+++ main.py
@@ -9,41 +9,29 @@ from invariant import InvariantModel, train_step
 model = InvariantModel()
 optimizer = tf.keras.optimizers.Adam()
 
-
-# Generate random regression data: y = 3x + noise
 N = 10_000
-gs = [9.81, 1.62, 3.71, 24.79, 274.0]
+# vrednosti podobne velikosti, brez večjih lukenj
+gs = [1.62, 2.11, 3.33, 3.71, 4.9, 7.11, 8.3, 9.81]
 x_train = freefall(gs, N=N)
-x_train = tf.convert_to_tensor(x_train)
-
 
 fig, axes = plt.subplots(3, 1, figsize=(24, 8))
-epochs = 30
+epochs = 30 # dobro bi bilo imeti to veliko večje recimo 100
 losses = []
 predict = []
 for epoch in range(epochs):
     loss = train_step(model, optimizer, x_train)
-    losses.append(loss)
     print(f"Epoch {epoch + 1}, Loss: {loss.numpy():.4f}")
-    losses.append(loss.numpy())
+    losses.append(loss)
 
+# narisana grafa g_est od g in sqrt(g_est) od g
+g_test = np.linspace(min(gs),max(gs))
+g_ests = []
+for g in g_test:
+    # izračunamo estimirano kostnanto, kot povprečje večih outputov
+    g_est = np.average(model(freefall([g], N=5000)[0,:,:])) 
+    g_ests.append(g_est)
 
-h = np.linspace(0, 3, 1000)
-x_test = np.array([h, np.ones(1000)]).T
-x_test = tf.convert_to_tensor(x_test)
-out_gsh = []
-for x in x_test:
-    out_gsh.append(model.call(np.array([x]))[0])
-
-t = np.linspace(0, 3, 1000)
-x_test = np.array([np.ones(1000), t]).T
-x_test = tf.convert_to_tensor(x_test)
-out_gst = []
-for x in x_test:
-    out_gst.append(model.call(np.array([x]))[0])
-
-print(model.call(np.array([[4.905, 1]]))[0])
 axes[0].plot(losses)
-axes[1].plot(h, out_gsh)
-axes[2].plot(t, out_gst)
+axes[1].plot(g_test, g_ests)
+axes[2].plot(g_test, np.sqrt(g_ests-min(g_ests)))
 plt.show()

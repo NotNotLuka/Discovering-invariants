d6581dbc34b7f891b658ccc84dfb365c230f27d2
ZetkoOfficial <info@zetkoofficial.com>
2025-07-16 17:41:11 +0200

    manjši popravki

--- invariant.py
+++ invariant.py
@@ -11,7 +11,7 @@ class InvariantModel(tf.keras.Model):
             tf.keras.layers.Dense(hidden_units, activation=mish),
             tf.keras.layers.Dense(hidden_units, activation=mish),
             tf.keras.layers.Dense(hidden_units, activation=mish),
-            tf.keras.layers.Dense(1),
+            tf.keras.layers.Dense(1, activation=mish),
         ]
 
     def call(self, inputs):
@@ -24,10 +24,10 @@ class InvariantModel(tf.keras.Model):
         loss = 0.0
         Q = 1
         for group in x:
-            print("group", group.shape)
+            #print("group", group.shape)
             Ftheta = self.call(group)
             Ftheta_epsilon = self.call(group + epsilon)
-            print("ftheta", Ftheta.shape)
+            #print("ftheta", Ftheta.shape)
             loss_group = tf.math.reduce_std(Ftheta) + tf.math.abs(
                 Q - tf.math.reduce_std(Ftheta_epsilon)
             )
@@ -41,6 +41,7 @@ def train_step(model, optimizer, x):
     with tf.GradientTape() as tape:
         epsilon = tf.random.normal(shape=x.shape[1:], mean=0.0, stddev=1.0, dtype=tf.float64)
         loss = model.compute_loss(x, epsilon)
-    grads = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        
     return loss

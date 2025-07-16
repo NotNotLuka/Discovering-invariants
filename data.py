862b089488af9e9e31d8169094ea90118dee228a
ZetkoOfficial <info@zetkoofficial.com>
2025-07-16 17:44:59 +0200

    prepreči negativnost

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ def freefall(gs, N=1000):
     T = 2
     for i in range(len(gs)):
         g = gs[i]
-        t = T + np.random.randn(N) * T
+        t = np.random.rand(N) * T
         h = 0.5 * g * np.power(t, 2)
         data[i, :, :] = np.array([h, t]).T
 
